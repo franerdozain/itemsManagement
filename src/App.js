c2876ae3c72e85cfa65a93b79b74eba40aefe946
Fran <franciscoerdozain22@gmail.com>
2023-06-29 13:19:13 -0300

    create components ItemsList & AddItemForm

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import ItemsList from './ItemsList';
+import AddItemForm from './AddItemForm';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [items, setItems] = useState([]);
+
+  const addItem = (item) => {
+    setItems((prevItems) => [...prevItems, item]);
+  };
+
+  return (  
+    <div>
+      <AddItemForm addItem={addItem} />
+      <ItemsList items={items} />
     </div>
   );
 }
